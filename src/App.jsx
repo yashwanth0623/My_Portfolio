ea90a73ea392016d5772a7dcb6b2a0a083d8f4c9
yashwanth0623 <163510383+yashwanth0623@users.noreply.github.com>
2025-08-19 21:50:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <Header />
+      
       <main>
         <section id="home">
           <Home />
